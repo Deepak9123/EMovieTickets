72309030fb6cf1db8203f98ec5ad7a14896d7031
Deepak Kumar Mahato <deepakmahatoj.n.v@gmail.com>
2024-02-10 13:34:50 +0530

    Add files via upload

---
+++ eMovietickets-master/eMovietickets/Controllers/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eMovietickets.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Profile()
+        {
+            return View();
+        }
+        public IActionResult FAQ()
+        {
+            return View();
+        }
+        public IActionResult More()
+        {
+            return View();
+        }
+        public IActionResult Games()
+        {
+            return View();
+        }
+    }
+}
